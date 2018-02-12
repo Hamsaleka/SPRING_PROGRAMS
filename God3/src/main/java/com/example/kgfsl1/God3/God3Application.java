1e353dff2264a65766743994cad73759df35220e
Hamsaleka <35956740+Hamsaleka@users.noreply.github.com>
2018-02-12 19:01:42 +0530

    Add files via upload

---
+++ God3/src/main/java/com/example/kgfsl1/God3/God3Application.java
@@ -0,0 +1,21 @@
+package com.example.kgfsl1.God3;
+
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class God3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(God3Application.class, args);
+	}
+	@Bean
+	public ServletRegistrationBean h2servletRegistration() {
+		ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
+		registration.addUrlMappings("/console/*");
+		return registration;
+	}
+}
